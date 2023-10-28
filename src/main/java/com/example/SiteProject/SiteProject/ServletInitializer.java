70548635c00140c01bff104aa177d57eab4ec350
GTonin <gustavottonin@hotmail.com>
2023-10-27 23:49:42 -0300

    lala

---
+++ src/main/java/com/example/SiteProject/SiteProject/ServletInitializer.java
@@ -0,0 +1,13 @@
+//package com.example.SiteProject.SiteProject;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(SiteProjectApplication.class);
+//    }
+//
+//}
