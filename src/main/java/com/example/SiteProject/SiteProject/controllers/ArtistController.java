6f45adb4307ced5417fa77ce906b1de0b6f26442
joaovitrp <joaovitorpavaor@hotmail.com>
2023-10-28 01:09:19 -0300

    criação da DTO e PostMapping

--- src/main/java/com/example/SiteProject/SiteProject/controllers/ArtistController.java
+++ src/main/java/com/example/SiteProject/SiteProject/controllers/ArtistController.java
@@ -1,21 +1,25 @@
 package com.example.SiteProject.SiteProject.controllers;
 
+import com.example.SiteProject.SiteProject.dtos.ArtistDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
-@RequestMapping(value = "/artist")
 @RequiredArgsConstructor
 public class ArtistController {
 
-    @GetMapping
-    public String getString(){
-        return "OK";
+    @PostMapping("/artist")
+    public ResponseEntity<ArtistDTO> crateArtist(@RequestBody ArtistDTO artist) {
+
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 
-}
+    }
+
+
+
