dc3c9a58194f30aa35366a1357f38d7c654ecb7c
GTonin <gustavottonin@hotmail.com>
2023-11-02 20:28:54 -0300

    CRUD completo Music

--- src/main/java/com/example/SiteProject/SiteProject/repositories/MusicRepository.java
+++ src/main/java/com/example/SiteProject/SiteProject/repositories/MusicRepository.java
@@ -1,11 +1,20 @@
 package com.example.SiteProject.SiteProject.repositories;
 
 import com.example.SiteProject.SiteProject.entities.MusicEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MusicRepository extends BaseRepository<MusicEntity> {
 
-    MusicEntity findByName(String name);
+    @Query("SELECT o FROM MusicEntity o WHERE genre in :genres " +
+            "AND (:name is NULL or o.name = :name) ")
+    Page<MusicEntity> findByGenre(@Param("genres") List<String> genres,
+                                  @Param("name") String name,
+                                  Pageable pageable);
 }
