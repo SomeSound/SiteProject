3987dae99e3078d3f68bb12eaa08083d919efd85
GTonin <gustavottonin@hotmail.com>
2023-12-02 20:42:35 -0300

    Ajustes track

--- src/main/java/com/example/hyper/entities/TrackEntity.java
+++ src/main/java/com/example/hyper/entities/TrackEntity.java
@@ -6,7 +6,6 @@ import lombok.*;
 
 import java.io.Serializable;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @Getter
 @Setter
@@ -17,8 +16,8 @@ import java.io.Serializable;
 public class TrackEntity extends BaseEntity implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MUSIC_SEQ")
-    @SequenceGenerator(name = "MUSIC_SEQ", sequenceName = "MUSIC_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TRACK_SEQ")
+    @SequenceGenerator(name = "TRACK_SEQ", sequenceName = "TRACK_SEQ", allocationSize = 1)
     private Long id;
 
     @Column(name = "NAME", nullable = false)
@@ -30,8 +29,8 @@ public class TrackEntity extends BaseEntity implements Serializable {
     @Column(name = "GENRE", nullable = false)
     private Genre genre;
 
-//    @Column(name = "REVIEW")
-//    private ReviewEntity reviewEntity;
+    @Column(name = "REVIEW")
+    private ReviewEntity reviewEntity;
 
 //    @Column(name = "ARTIST") // CRIAR FOREIGN-KEY APONTANDO PARA ARTISTA
 //    private ArtistEntity Artist;
